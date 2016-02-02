d2ef2de1feb5ba6ecdda105ccf7de2edbbe2e3da
Nicolas Bougie <bougie.nicolas@gmail.com>
2016-02-02 18:27:53 +0100

    d

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import gzip
 import pickle
 import matplotlib.pyplot as plt
 from pprint import pprint
+import operator
+from itertools import islice
 
 with gzip.open('mnist.pkl.gz','rb','latin-1') as ifile:
     train_set,valid_set,test_set = pickle.load(ifile)
@@ -22,7 +24,7 @@ def show_image2(tab):
 
 
 #show_image(0,0)
-pprint(train_set[0][0])
+#pprint(train_set[0][0])
 # train set => matrice contenant les images et deuxieme elemeent est l etiquette associee
 #print(valid_set)
 
@@ -47,7 +49,7 @@ def binarize_short(tableau):
                 tableau[e] =0
 
 
-binarize_short(train_set[0][0])
+#binarize_short(train_set[0][0])
 
 
 def save_tagger(filename,tag):
@@ -135,4 +137,129 @@ def moyenne_egal(val):
     ma_moyenne  = moyennes(liste_appel)
     show_image2(ma_moyenne)
 
-moyenne_egal(4)
+
+binarize(train_set[0])
+binarize(test_set[0][:1000])
+#moyenne_egal(4)
+
+# espace euclident => distance entre points
+# plus des points sont proches plus ils sont similaires
+# attribuer l etiquette de l exemple le plus proche 
+# pour etre plus robuste faire moyenne des etiquettes des k voisins les plus proches
+# complexites : N * d (nombre exemples * nombre de dimensions) pour classifier un point
+
+
+#DIstance minimale
+# determiner un representant de chaque classe en utilisant une moyenne
+#attribuer une etiquette de la classe dont representant est plus proche
+
+def sortes(ma_list):
+    result = sorted(ma_list.items(), key=operator.itemgetter(1))
+    return result
+
+
+
+def distance(a,b):
+    return np.linalg.norm(a-b)
+
+def get_k_nearest_voisins(k,liste,x):
+    lis = {}
+    for i in range(len(liste)):
+        lis[i] = distance(x,liste[i])
+    return (sortes(lis)[:k])
+
+
+def prediction_k(image_to_predict,k,train_datas,labels):
+    voisons_proche = get_k_nearest_voisins(k,train_datas,image_to_predict)
+    compte= [0 for i in range(0,10)]
+    for e,v in voisons_proche:
+        compte[labels[e]]+=1
+    #print("WALLAH",compte,"THUG",compte.index(max(compte)))
+    return (compte.index(max(compte)))
+    
+
+def prediction(ids,k):
+    print("PREDICTION")
+    print("VALEURS REELLES : ", test_set[1][ids])
+    print("VALEURS ESTIMMEESS : ",prediction_k(test_set[0][ids],k, train_set[0],train_set[1])) 
+
+
+#prediction(10,2)
+
+
+def prediction_globale(nombre):
+   x = [i for i in range(1,11)]
+   y = [0 for i in range(1,11)]
+   for k in x:
+       print(" k :" , k)
+       for ids in range(nombre):
+           vals = prediction_k(test_set[0][ids],k, train_set[0],train_set[1])
+           #print(test_set[1][ids] , "valeur predite :" , vals)
+           if(test_set[1][ids] != vals):
+               y[k-1] +=1
+               #print("erreur : id : ", ids,  " k :" , k ," predite :", vals, " reele :",test_set[1][ids])  
+   for i in range(len(y)):
+       y[i]  = (y[i] /float(nombre))*100.0
+   plt.plot(x,y)
+   plt.xlabel('K nearest neightbours')
+   plt.ylabel('Error rate (%)')
+   plt.savefig('full_train.png')
+   plt.show()
+
+prediction_globale(150)
+
+
+#----------------------------------------------------------------
+
+def moyenne_des_points(nbsss):
+    x = [[0 for  j in range(0,784)] for i in range(0,10)]
+    nb = [0 for i in range(0,10)]
+    for i in range(nbsss):
+        nb[train_set[1][i]]+=1
+        x[train_set[1][i]] = np.add(x[train_set[1][i]],train_set[0][i])
+    for i in range(len(x)):
+        x[i] = np.divide(x[i],nb[i])
+    return x
+
+
+# tout faire : len(train_set[0])
+points_moy = moyenne_des_points(len(train_set[0]))
+
+def test_moy(points_moy):
+    nb_Erreurs = 0
+    count = 0
+    for i in range(len(test_set[0])):
+        mins = 1000000000
+        val = -1
+        for e in range(len(points_moy)):
+            calc = distance(test_set[0][i],points_moy[e])
+            if(calc < mins):
+                mins = calc
+                val = e
+        count +=1
+        print("vraie !" , test_set[1][i], "estimate : ",val)
+        if(test_set[1][i] != val):
+            nb_Erreurs +=1
+    print("Error rate : ", nb_Erreurs/float(count))
+
+
+
+
+
+#('Error rate : ', 0.1832)
+#test_moy(points_moy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
